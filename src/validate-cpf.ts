aa5b25362c747b2eb5dd208db1abb5545ee3b584
ocsoares <casoares016@gmail.com>
2023-03-03 19:44:27 -0300

    feat: add punctuation validation

--- src/validate-cpf.ts
+++ src/validate-cpf.ts
@@ -1,5 +1,11 @@
 import { cpf } from 'cpf-cnpj-validator';
 
 export function isValidCPF(value: string): boolean {
+    // Com isso, só é permitido o CPF no formato EXATO de pontuação ou um
+    // CPF válido sem nenhuma pontuação
+    if (!/^\d{3}(\.?\d{3}){2}-?\d{2}$/.test(value)) {
+        return false;
+    }
+
     return cpf.isValid(value, true);
 }
